68e5f008e754da7c07dc0ca625a8686f5235be24
Adlercohen <85706777+fiifidawson@users.noreply.github.com>
2022-12-14 22:15:49 +0000

    Update loading.dart

--- flutter/world_time_app/lib/pages/loading.dart
+++ flutter/world_time_app/lib/pages/loading.dart
@@ -10,20 +10,31 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
-  void getData() async {
-    var url = Uri.parse('https://jsonplaceholder.typicode.com/todos/1');
+  void getTime() async {
+    // Make request
+
+    var url = Uri.parse('https://worldtimeapi.org/api/timezone/Europe/London');
     var response = await http.get(url);
     Map data = jsonDecode(response.body); // this requires import dart:convert
-    print(
-        data); // result: {userId: 1, id: 1, title: delectus aut autem, completed: false}
-    print(data['title']); // result: delectus aut autem
+
+    // get properties from data
+    String datetime = data['datetime'];
+    String offset = data['utc_offset'].substring(1, 3);
+
+    //print(datetime);
+    //print(offset);
+
+    // create DateTime object
+    DateTime now = DateTime.parse(datetime);
+    now = now.add(Duration(hours: int.parse(offset)));
+    print(now);
   }
 
   int counter = 0;
   @override
   void initState() {
     super.initState();
-    getData();
+    getTime();
   }
 
   @override
