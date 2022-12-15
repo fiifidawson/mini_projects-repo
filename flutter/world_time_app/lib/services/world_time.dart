cf5ed9225fce73ddaeb95323a4dadfd5b5613a0f
Adlercohen <85706777+fiifidawson@users.noreply.github.com>
2022-12-15 00:45:51 +0000

    Update world_time.dart

--- flutter/world_time_app/lib/services/world_time.dart
+++ flutter/world_time_app/lib/services/world_time.dart
@@ -3,10 +3,10 @@ import 'package:http/http.dart';
 import 'dart:convert';
 
 class WorldTime {
-  String location = ""; // location name for the UI
-  String time = ""; // the time in that location
-  String flag = ""; // url to an asset flag icon
-  String url = ""; // location url for api endpoint
+  late String location; // location name for the UI
+  late String time; // the time in that location
+  late String flag; // url to an asset flag icon
+  late String url; // location url for api endpoint
 
   WorldTime({required this.location, required this.flag, required this.url});
 
