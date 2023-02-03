eee9ce028a75e91f5eb2775e53b4eef331c1af2a
Fiifi Dawson <edem.fiifi.dawson@gmail.com>
2023-02-03 15:00:04 +0000

    Update

--- flutter/hive_practice/lib/home_page.dart
+++ flutter/hive_practice/lib/home_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,8 +10,48 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  // reference our box
+  final _myBox = Hive.box('myBox');
+
+  // write data
+  void writeData() {
+    _myBox.put(1, 'John Doe');
+  }
+
+  // read data
+  void readData() {
+    _myBox.get(1);    
+  }
+
+  // delete data
+  void deleteData() {
+    _myBox.delete(1);
+}
+
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
-  }
+    return Scaffold(
+        body: Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                MaterialButton(onPressed: writeData,
+                color: Colors.blue,
+                child: const Text("Write"),
+                ),
+                MaterialButton(onPressed: readData,
+                color: Colors.blue,
+                child: const Text("Read"),
+                ),
+                MaterialButton(onPressed: deleteData,
+                color: Colors.blue,
+                child: const Text("Delete"),
+              ),
+            ],
+          )
+        )
+      );
+    }
 }
--- flutter/hive_practice/lib/main.dart
+++ flutter/hive_practice/lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:hive_practice/home_page.dart';
 
-void main() {
+Future<void> main() async {
 
   // Initialize Hive
+  await Hive.initFlutter();
 
-  
+  //open the box
+  var box = await Hive.openBox('myBox');
+
+
   runApp(const MyApp());
 }
 
